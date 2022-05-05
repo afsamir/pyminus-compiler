0f8f361cee59540553e05a08d811cdd01de1c631
Amir Afsari <afsari@ce.sharif.edu>
2022-05-05 18:36:33 +0430

    parse table reversed

--- parser.py
+++ parser.py
@@ -7,7 +7,32 @@ class Parser:
         self.parse_table = parse_table
         self.parse_stack = ['Program']
         self.tree_rules = []
+        self.errors = []
         self.get_next_input()
+
+
+    def parse(self):
+        while(len(self.parse_stack) is not 0 and self.current_token[0] is not '$'):
+            self.proceed()
+
+
+    def proceed(self):
+        print(self.current_token, self.parse_stack)
+        if(self.current_token[0] == self.parse_stack[-1]):
+            self.accept_token()
+        elif self.parse_stack[-1] == 'Epsilon':
+            self.parse_stack.pop()
+        else:
+            try:
+                move = self.parse_table[self.parse_stack[-1]][self.current_token[0]]
+                if(move):
+                    if(move == 'Synch'):
+                        self.handle_synch_error()
+                    else:
+                        self.apply_rule(move[::-1])
+            except :
+                self.handle_empty_error()
+
 
     def get_next_input(self):
         token = self.scanner.get_next_token()
@@ -23,28 +48,18 @@ class Parser:
         self.tree_rules.append(rule_tokens[::-1])
 
     def accept_token(self):
-        terminal = self.parse_stack.pop()
+        self.parse_stack.pop()
         self.tree_rules.append(self.current_token[1])
         print(self.current_token)
         self.get_next_input()
 
-    def proceed(self):
-        # print('__________________________________________________________________________')
-        print(self.current_token, self.parse_stack)
-        # print(self.current_token[0])
-        # print(self.parse_table[self.current_token[0]])
-        # print('__________________________________________________________________________')
-        if(self.current_token[0] == self.parse_stack[-1]):
-            self.accept_token()
-        elif self.parse_stack[-1] == 'Epsilon':
-            self.parse_stack.pop()
-        else:
-            move = self.parse_table[self.current_token[0]][self.parse_stack[-1]]
-            if(move):
-                self.apply_rule(move[::-1])
-            else:
-                raise Exception()
+    def handle_synch_error(self):
+        term = self.parse_stack.pop()
+        error = (self.scanner.line_no, 'missing' + str(term))
+        self.errors.append(error)
 
-    def parse(self):
-        while(len(self.parse_stack) is not 0 and self.current_token[0] is not 'EOF'):
-            self.proceed()
+    def handle_empty_error(self):
+        error = (self.scanner.line_no, 'missing')
+        self.errors.append(error)
+        self.get_next_input()
+
